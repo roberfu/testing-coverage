2823721311a81a979ec236dc2a15b4e1670d5af3
Roberto Fuentes <roberfuentesg@gmail.com>
2024-03-16 12:57:54 -0300

    Update repository test

--- src/test/java/cl/springmachine/api/rest/spring/boot/repository/PokemonRepositoryTest.java
+++ src/test/java/cl/springmachine/api/rest/spring/boot/repository/PokemonRepositoryTest.java
@@ -2,6 +2,7 @@ package cl.springmachine.api.rest.spring.boot.repository;
 
 import cl.springmachine.api.rest.spring.boot.model.Pokemon;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
@@ -9,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
-import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -18,69 +18,8 @@ class PokemonRepositoryTest {
     @Autowired
     private PokemonRepository pokemonRepository;
 
-    @Test
-    void save() {
-        Pokemon pokemon = Pokemon.builder()
-                .name("pikachu")
-                .type("electric")
-                .build();
-
-        Pokemon saved = pokemonRepository.save(pokemon);
-
-        Assertions.assertNotNull(saved);
-        Assertions.assertTrue(saved.getId() > 0);
-
-    }
-
-    @Test
-    void findAll() {
-        Pokemon first = Pokemon.builder()
-                .name("pikachu")
-                .type("electric")
-                .build();
-
-        Pokemon second = Pokemon.builder()
-                .name("gloom")
-                .type("grass")
-                .build();
-
-        pokemonRepository.save(first);
-        pokemonRepository.save(second);
-
-        List<Pokemon> list = pokemonRepository.findAll();
-
-        Assertions.assertNotNull(list);
-        Assertions.assertEquals(2, list.size());
-
-    }
-
-    @Test
-    void findById() {
-        Pokemon pokemon = Pokemon.builder()
-                .name("pikachu")
-                .type("electric")
-                .build();
-
-        Pokemon saved = pokemonRepository.save(pokemon);
-
-        Optional<Pokemon> optional = pokemonRepository.findById(saved.getId());
-
-        Assertions.assertTrue(optional.isPresent());
-    }
-
-    @Test
-    void delete() {
-        Pokemon pokemon = Pokemon.builder()
-                .name("pikachu")
-                .type("electric")
-                .build();
-
-        Pokemon saved = pokemonRepository.save(pokemon);
-        pokemonRepository.deleteById(saved.getId());
-        Optional<Pokemon> optional = pokemonRepository.findById(saved.getId());
-
-        Assertions.assertTrue(optional.isEmpty());
-
+    @BeforeEach
+    void setUp() {
     }
 
     @Test
@@ -101,8 +40,5 @@ class PokemonRepositoryTest {
         List<Pokemon> list = pokemonRepository.findAllByType("electric");
 
         Assertions.assertEquals(2, list.size());
-
     }
-
-
 }
